61a4ca5e71be8756e3adba9ea06d70cc62df13cf
JamesMcPeek <50390312+JamesMcPeek@users.noreply.github.com>
2021-11-22 13:02:19 -0500

    Add files via upload

---
+++ call-server.js
@@ -0,0 +1,107 @@
+let outputArea;   //globally available to all functions
+let outputMessage;
+
+//----------------------testing basic GET functionality
+function handleReadButton() {
+    const name = document.querySelector('#name').value;
+    const note = document.querySelector('#note').value;
+
+
+    const url = "http://localhost:3000";
+    const params = `?name=${name}&note=${note}`;
+
+    const fetchObject = {
+        method: 'GET',
+        headers: {
+            'Content-Type' : 'text/html'
+        }
+    };
+
+    //perform fetch on url with parameters (query String on GET)
+    fetch(url + params, fetchObject)
+        .then(response => response.json())               // obtain json object sent from server
+        .then(jsonObject => {                            // use jsonObject and get its message property
+            outputMessage.innerHTML = jsonObject.message;   // set innerHTML of Area to message sent in jsonObject
+        });
+
+}//end handleBasicButton
+//---------------------------------------------------------
+
+//-------------------------send message to server to write to file 
+function handleWriteButton() {
+    const name = document.querySelector('#name').value;
+    const note = document.querySelector('#note').value;
+
+    const url = "http://localhost:3000/write-file";
+
+    const dataObject = {
+        name: name,
+        note: note
+    };
+
+    const fetchObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(dataObject)
+    };
+
+    //perform fetch on url with parameters (query String on GET)
+    fetch(url, fetchObject)
+        .then(response => response.json())               // obtain json object sent from server
+        .then(jsonObject => {                            // use jsonObject and get its message property
+            outputMessage.innerHTML = jsonObject.message;   // set innerHTML of Area to message sent in jsonObject
+        });
+
+}//end handleWriteButton
+
+//-------------------------send message to server to delete to file 
+function handleDeleteButton() {
+    const name = document.querySelector('#name').value;
+    const note = document.querySelector('#note').value;
+
+    const url = "http://localhost:3000/delete-file";
+
+    const dataObject = {
+        name: name,
+        note: note
+    };
+
+    const fetchObject = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(dataObject)
+    };
+
+    //perform fetch on url with parameters (query String on GET)
+    fetch(url, fetchObject)
+        .then(response => response.json())               // obtain json object sent from server
+        .then(jsonObject => {                            // use jsonObject and get its message property
+            outputMessage.innerHTML = jsonObject.message;   // set innerHTML of Area to message sent in jsonObject
+        });
+
+}//end handleDeleteButton
+
+//----------------------------------------------------------------
+function start() {
+    const readButton = document.querySelector('#writeBtn');
+    const writeButton = document.querySelector('#writeBtn');
+    const deleteButton = document.querySelector('#deleteBtn');
+
+    readButton.onclick = handleReadButton;
+    writeButton.onclick = handleWriteButton;
+    deleteButton.onclick = handleDeleteButton;
+
+    //initialize global outputArea
+    outputArea = document.querySelector('#output');
+    outputMessage = document.querySelector('#message');
+}//end start
+
+window.onload = start;
+
+// Read -> GET
+// Write -> POST
+// Delete -> DELETE
---
+++ index.js
@@ -0,0 +1,70 @@
+//make sure to run on terminal:
+//   npm i express
+//   npm i cors
+//   npm i body-parser   [optional]
+//otherwise these modules will not work!
+
+const express = require('express');
+const app = express();  //get an express object
+const cors = require('cors');  //avoid that nasty CORS error
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const { fileURLToPath } = require('url');
+const { response } = require('express');
+
+const portNum = 3000;
+
+//take care of CORS situation
+app.use(cors({origin: '*'}));
+
+//allow body parsing
+app.use(express.json());
+
+app.use(express.static(__dirname));
+
+//second argument is a callback function
+//-- this is the function that will be called when 
+//   there is an HTTP GET request for this endpoint
+app.get('/', (req, res) => {
+    console.log('\n\nON THE SERVER');
+    console.log('received from client: ' + req.query.name + ' ' + req.query.note);
+    console.log('sending response to the client from / ...');
+
+    res.send(JSON.stringify({message : "reading file"}));
+});
+
+app.post('/write-file', (req, res) => {
+    console.log('\n\nON THE SERVER');
+    console.log('writing to file: ' + req.body.name + ' ' + req.body.note);
+    console.log('sending response to client from /write-file ...');
+
+    const file = req.body.name + ".txt";
+    const note = req.body.note + "\n";
+
+    fs.appendFile(file, note, (err) => {
+        if (err) {throw err;}
+        console.log("wrote file");
+    })
+
+    res.send(JSON.stringify({message: 'Your note has been recorded'}));
+});
+
+app.delete('/delete-file', (req, res) => {
+    console.log('\n\nON THE SERVER');
+    console.log('deleting the file: ' + req.body.name + '.txt');
+    console.log('sending response to client from /delete-file ...');
+
+    const file = req.body.name + ".txt";
+
+    fs.unlink(file, (err) => {
+        if (err) {throw err;}
+        console.log("deleted file");
+    })
+
+    res.send(JSON.stringify({message: 'Your note has perished in the depths of Moria'}));
+});
+
+//make server listen on a port
+app.listen(portNum, () => {
+    console.log(`listening on port ${portNum}`);
+});

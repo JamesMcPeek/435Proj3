ac51eec0d57c1bece8c6a8f7a0cf58179ce2b9fb
JamesMcPeek <50390312+JamesMcPeek@users.noreply.github.com>
2021-11-24 22:43:53 -0500

    Updated with update-file

--- index.js
+++ index.js
@@ -20,21 +20,7 @@ app.use(express.json());
 
 app.use(express.static(__dirname));
 
-//second argument is a callback function
-//-- this is the function that will be called when 
-//   there is an HTTP GET request for this endpoint
-app.get('/', (req, res) => {
-    console.log('\n\nON THE SERVER');
-    console.log('received from client: ' + req.query.name + ' ' + req.query.note);
-    console.log('sending response to the client from / ...');
-
-    const file = req.query.name + ".txt";
-
-    var content = fs.readFileSync(file).toString();
-
-    res.send(JSON.stringify({message : `${content}`}));
-});
-
+//Create file
 app.post('/write-file', (req, res) => {
     console.log('\n\nON THE SERVER');
     console.log('writing to file: ' + req.body.name + ' ' + req.body.note);
@@ -51,6 +37,40 @@ app.post('/write-file', (req, res) => {
     res.send(JSON.stringify({message: 'Your note has been recorded'}));
 });
 
+//Read file
+app.get('/', (req, res) => {
+    console.log('\n\nON THE SERVER');
+    console.log('received from client: ' + req.query.name + ' ' + req.query.note);
+    console.log('sending response to the client from / ...');
+
+    const file = req.query.name + ".txt";
+
+    var content = fs.readFileSync(file).toString();
+
+    res.send(JSON.stringify({message : `${content}`}));
+});
+
+//Update file
+app.put('/update-file', (req,res) => {
+    console.log('\n\nON THE SERVER');
+    console.log('updating file: ' + req.body.name + ' ' + req.body.note);
+    console.log('sending response to client from /update-file ...');
+    
+    const file = req.body.name + ".txt";
+    const note = req.body.note;
+
+    var content = fs.readFileSync(file).toString();
+    let noteToDelete = content.replace(note, "");
+
+    fs.writeFile(file, noteToDelete, (err) => {
+        if (err) {throw err;}
+        console.log("updated file");
+    })
+
+    res.send(JSON.stringify({message : `deleted ${note} from notes`}))
+})
+
+//Delete file
 app.delete('/delete-file', (req, res) => {
     console.log('\n\nON THE SERVER');
     console.log('deleting the file: ' + req.body.name + '.txt');
